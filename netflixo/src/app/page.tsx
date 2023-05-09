cd43e7dd57e43c76769d6b5f5ac60dbf9eb1db41
Muhammad Samiullah <itzsamiullah1@gmail.com>
2023-05-09 20:23:25 +0500

    Name changed

--- netflixo/src/app/page.tsx
+++ netflixo/src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="h-header bg-main w-full">
       <h1 className="text-h1 text-star font-bold underline">
-        Hello world!
+        Hello Sami!
       </h1>
     </div>
   )
